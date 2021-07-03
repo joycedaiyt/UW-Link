88b80d51b9240846a3e035234a9a37836ec5d426
Alan Wang <1967878682w@gmail.com>
2021-07-03 23:59:08 +0800

    Created html for profile page

--- uwlink/routes.py
+++ uwlink/routes.py
@@ -4,7 +4,7 @@ from flask import Blueprint, jsonify, request, render_template, flash, redirect,
 from flask_login import UserMixin, current_user, login_required, login_user
 from mongoengine.errors import DoesNotExist
 from uwlink import login_manager
-from uwlink.forms import LoginForm, SignupForm, EventForm, SearchForm
+from uwlink.forms import EventForm, SearchForm
 from uwlink.models import User, Event, Tag
 from werkzeug.security import check_password_hash, generate_password_hash
 from bson.objectid import ObjectId
@@ -34,27 +34,32 @@ def user_loader(user_id):
 # https://en.wikipedia.org/wiki/Cryptographic_hash_function#Password_verification
 @routes.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = SignupForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data,
-                    email=form.email.data,
+    form = request.form
+    if request.method == 'POST':
+        try:
+            if User.objects.get(username=form.get("signupUser")):
+                return "User name already exist"
+        except DoesNotExist:
+            pass
+        user = User(username=form.get("signupUser"),
+                    email=form.get("signupEmail"),
                     events_joined=[],
                     events_created=[],
                     joined_at=datetime.now(),
-                    hashed_password=generate_password_hash(form.password.data))
+                    hashed_password=generate_password_hash(form.get("signupPassword")))
         user.save()
         flash('You have been signed up!')
         return redirect(url_for('.login'))
-    return render_template('signup.html', form=form)
+    return render_template('login.html', form=form)
 
 
 @routes.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
+    form = request.form
+    if request.method == 'POST':
         try:
-            user = User.objects.get(username=form.username.data)
-            if check_password_hash(user.hashed_password, form.password.data):
+            user = User.objects.get(username=form.get("loginUser"))
+            if check_password_hash(user.hashed_password, form.get("loginPassword")):
                 user = LoginUser(user)
                 login_user(user)
                 return "logged in"
@@ -148,8 +153,8 @@ def profile():
     for event_id in current_user.user.events_joined:
         event = Event.objects.get(id = event_id)
         events_joined.append(event)
-    return render_template('profile.html', name = current_user.username,
-                           email = current_user.email,
-                           join = current_user.joined_at,
+    return render_template('profile.html', name = current_user.user.username,
+                           email = current_user.user.email,
+                           join = current_user.user.joined_at,
                            events_created = events_created,
                            events_joined = events_joined)
